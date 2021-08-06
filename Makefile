1cca1724243a6cf79dccf9b9a81267f9975f8d14
Martin Rusev <martin@amon.cx>
2021-08-06 21:20:14 +0200

    Add mariadb server

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+pre_deploy:
+	sudo apt install python3-pip python3-wheel python3-setuptools -y
+
+# microk8s enable registry
 deploy:
-	charmcraft pack
+	charmcraft pack --destructive-mode
 	juju deploy ./mariadb-galera-operator_ubuntu-20.04-amd64.charm --resource galera-image=localhost:32000/mariadb-galera:latest
 
+d:
+	juju deploy ./mariadb-galera-operator_ubuntu-20.04-amd64.charm --resource galera-image=localhost:32000/mariadb-galera:latest
 
 remove:
 	juju remove-application galera --force
@@ -10,5 +16,6 @@ remove:
 juju_reset:
 	juju destroy-controller galera --destroy-all-models --destroy-storage
 
+# microk8s enable dns
 juju_setup:
 	juju bootstrap microk8s galera
--- src/charm.py
+++ src/charm.py
@@ -6,9 +6,18 @@ import logging
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import (
+    ActiveStatus,
+    MaintenanceStatus,
+    ModelError,
+    WaitingStatus,
+)
+
 
 logger = logging.getLogger(__name__)
+
+PEER = "galera"
+MYSQL_PORT = 3306
 
 
 class MariaDBGaleraOperatorCharm(CharmBase):
@@ -18,7 +27,69 @@ class MariaDBGaleraOperatorCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.galera_pebble_ready, self._on_galera_pebble_ready)
+        self._stored.set_default(
+            mysql_initialized=False,
+            pebble_ready=False,
+        )
+        self.framework.observe(self.on.mysql_pebble_ready, self._on_pebble_ready)
+        self._provide_mysql()
+        self.container = self.unit.get_container(PEER)
+
+    ##############################################
+    #           CHARM HOOKS HANDLERS             #
+    ##############################################
+    def _on_pebble_ready(self, _):
+        self._stored.pebble_ready = True
+        self._update_peers()
+        self._configure_pod()
+
+    def _on_config_changed(self, _):
+        """Set a new Juju pod specification"""
+        self._update_peers()
+        self._configure_pod()
+
+    def _on_update_status(self, _):
+        """Set status for all units
+        Status may be
+        - MySQL is not ready,
+        - MySQL is not Initialized
+        - Unit is active
+        """
+
+        if not self.mysql.is_ready():
+            status_message = "MySQL not ready yet"
+            self.unit.status = WaitingStatus(status_message)
+            return
+
+        if not self._is_mysql_initialized():
+            status_message = "MySQL not initialized"
+            self.unit.status = WaitingStatus(status_message)
+            return
+
+        self.unit.status = ActiveStatus()
+
+    ##############################################
+    #               PROPERTIES                   #
+    ##############################################
+    @property
+    def mysql(self) -> MySQL:
+        """Returns MySQL object"""
+        peers_data = self.model.get_relation(PEER).data[self.app]
+        mysql_config = {
+            "app_name": self.model.app.name,
+            "host": self.unit_ip,
+            "port": MYSQL_PORT,
+            "user_name": "root",
+            "mysql_root_password": peers_data["mysql_root_password"],
+        }
+        return MySQL(mysql_config)
+
+    @property
+    def unit_ip(self) -> str:
+        """Returns unit's IP"""
+        if bind_address := self.model.get_binding(PEER).network.bind_address:
+            return str(bind_address)
+        return ""
 
     def _on_galera_pebble_ready(self, event):
         container = event.workload
@@ -39,7 +110,6 @@ class MariaDBGaleraOperatorCharm(CharmBase):
         container.add_layer("galera", pebble_layer, combine=True)
         container.autostart()
         self.unit.status = ActiveStatus()
-
 
 
 if __name__ == "__main__":
