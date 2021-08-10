d0600e30ace678842778c01ef729e1c1c997db5a
Martin Rusev <martin@amon.cx>
2021-08-10 21:55:21 +0200

    Add initial tests

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -1,48 +1,47 @@
-# Copyright 2021 Minikube
+# Copyright 2020 Canonical Ltd.
 # See LICENSE file for licensing details.
-#
-# Learn more about testing at: https://juju.is/docs/sdk/testing
 
 import unittest
-from unittest.mock import Mock
-
-from charm import MariaDBGaleraOperatorCharm
-from ops.model import ActiveStatus
 from ops.testing import Harness
+from ops.model import (
+    ActiveStatus,
+    WaitingStatus,
+)
+from charm import MariaDBGaleraOperatorCharm
+from unittest.mock import patch
 
 
 class TestCharm(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.harness = Harness(MariaDBGaleraOperatorCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
+        self.harness.add_oci_resource("galera-image")
 
-    def test_httpbin_pebble_ready(self):
-        # Check the initial Pebble plan is empty
-        initial_plan = self.harness.get_container_pebble_plan("galera")
-        self.assertEqual(initial_plan.to_yaml(), "{}\n")
-        # Expected plan after Pebble ready with default config
-        expected_plan = {
-            "services": {
-                "galera": {
-                    "override": "replace",
-                    "summary": "galera",
-                    "command": ".....",
-                    "startup": "enabled",
-                    "environment": {},
-                }
-            },
+    def test_pebble_layer_is_dict(self):
+        self.harness.set_leader(True)
+        config = {}
+        relation_id = self.harness.add_relation("galera", "galera")
+        self.harness.add_relation_unit(relation_id, "galera/1")
+        self.harness.update_config(config)
+        layer = self.harness.charm._build_pebble_layer()
+        self.assertIsInstance(layer["services"]["galera"], dict)
+
+    def test_pebble_layer_with_custom_config(self):
+        self.harness.set_leader(True)
+        config = {
+            "mysql_root_password": "PASS!",
+            "mysql_user": "RandomUser",
+            "mysql_password": "MYSQLPASS!",
+            "mysql_database": "db_galera",
         }
-        # Get the httpbin container from the model
-        container = self.harness.model.unit.get_container("galera")
-        # Emit the PebbleReadyEvent carrying the httpbin container
-        self.harness.charm.on.httpbin_pebble_ready.emit(container)
-        # Get the plan now we've run PebbleReady
-        updated_plan = self.harness.get_container_pebble_plan("galera").to_dict()
-        # Check we've got the plan we expected
-        self.assertEqual(expected_plan, updated_plan)
-        # Check the service was started
-        service = self.harness.model.unit.get_container("galera").get_service("galera")
-        self.assertTrue(service.is_running())
-        # Ensure we set an ActiveStatus with no message
-        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+        relation_id = self.harness.add_relation("galera", "galera")
+        self.harness.add_relation_unit(relation_id, "galera/1")
+        self.harness.update_config(config)
+        env = self.harness.charm._build_pebble_layer()["services"]["galera"][
+            "environment"
+        ]
+        self.assertEqual(env["MYSQL_ROOT_PASSWORD"], config["mysql_root_password"])
+        self.assertEqual(env["MYSQL_USER"], config["mysql_user"])
+        self.assertEqual(env["MYSQL_PASSWORD"], config["mysql_password"])
+        self.assertEqual(env["MYSQL_DATABASE"], config["mysql_database"])
